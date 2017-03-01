1599b6dddad96408076c0ed8f83223019c46bfbb
Korneliusz Rabczak <nikom1337@gmail.com>
2017-03-01 20:31:32 +0100

    name refactoring, trim request command key

--- src/main/java/com/thecookiezen/infrastructure/RequestFactory.java
+++ src/main/java/com/thecookiezen/infrastructure/RequestFactory.java
@@ -10,21 +10,21 @@ import java.util.stream.Collectors;
 public class RequestFactory {
 
     private static final String COMMAND = "command";
-    private static final String POINTERS = "pointers";
+    private static final String COORDINATE = "coordinate";
     private static final String PARAMETER = "parameter";
 
     private static final Pattern REQUEST_PATTERN = Pattern.compile("^(?<" + COMMAND +
-            ">[A-Z])\\s?(?<" + POINTERS + ">[\\d\\s]*)\\s?(?<" + PARAMETER + ">[a-z]?)$");
+            ">[A-Z])\\s?(?<" + COORDINATE + ">[\\d\\s]*)\\s?(?<" + PARAMETER + ">[a-z]?)$");
 
     private static final Pattern WHITESPACE_PATTERN = Pattern.compile("\\s");
 
     public Request createRequestFromString(String requestStringify) {
         final Matcher requestMatcher = REQUEST_PATTERN.matcher(requestStringify);
         if (requestMatcher.find()) {
-            final List<Integer> pointers = WHITESPACE_PATTERN.splitAsStream(requestMatcher.group(POINTERS))
+            final List<Integer> pointers = WHITESPACE_PATTERN.splitAsStream(requestMatcher.group(COORDINATE))
                     .map(Integer::parseInt)
                     .collect(Collectors.toList());
-            return new Request(requestMatcher.group(COMMAND).charAt(0), pointers, requestMatcher.group(PARAMETER));
+            return new Request(requestMatcher.group(COMMAND).trim().charAt(0), pointers, requestMatcher.group(PARAMETER));
         }
         return Request.EMPTY_REQUEST;
     }
