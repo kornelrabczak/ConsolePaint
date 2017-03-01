1e46acdd07592bbf56575c6bc2cbc322c9a00ec1
Korneliusz Rabczak <nikom1337@gmail.com>
2017-03-01 18:45:55 +0100

    adds tests for rectangle drawing

--- src/test/java/com/thecookiezen/infrastructure/SystemOutPrinterTest.java
+++ src/test/java/com/thecookiezen/infrastructure/SystemOutPrinterTest.java
@@ -4,6 +4,7 @@ import com.thecookiezen.bussiness.boundary.Printer;
 import com.thecookiezen.bussiness.control.Canvas;
 import com.thecookiezen.bussiness.entity.Line;
 import com.thecookiezen.bussiness.entity.Point;
+import com.thecookiezen.bussiness.entity.Rectangle;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -31,6 +32,7 @@ public class SystemOutPrinterTest {
 
     @Test
     public void should_print_canvas_with_vertical_and_horizontal_line() {
+        // given
         final Canvas canvas = new Canvas(10, 5);
 
         final Line vertical = new Line(new Point(7, 1), new Point(7, 7));
@@ -50,6 +52,80 @@ public class SystemOutPrinterTest {
                 "|   x   x  |" + System.lineSeparator() +
                 "|   x   x  |" + System.lineSeparator() +
                 "|   x   x  |" + System.lineSeparator() +
+                "|xxxxxxxxxx|" + System.lineSeparator() +
+                "|   x      |" + System.lineSeparator()  +
+                "------------" + System.lineSeparator() );
+    }
+
+    @Test
+    public void should_print_canvas_with_rectangle() {
+        // given
+        final Canvas canvas = new Canvas(10, 5);
+
+        final Rectangle rectangle = new Rectangle(new Point(3, 4), new Point(7, 1));
+
+        canvas.drawRectangle(rectangle);
+
+        // when
+        printer.print(canvas);
+
+        // then
+        assertThat(outContent.toString()).isEqualTo(
+                "------------" + System.lineSeparator() +
+                "|   xxxxx  |" + System.lineSeparator() +
+                "|   x   x  |" + System.lineSeparator() +
+                "|   x   x  |" + System.lineSeparator() +
+                "|   xxxxx  |" + System.lineSeparator() +
+                "|          |" + System.lineSeparator()  +
+                "------------" + System.lineSeparator() );
+    }
+
+    @Test
+    public void should_print_canvas_with_rectangle_beyond_boundary() {
+        // given
+        final Canvas canvas = new Canvas(10, 5);
+
+        final Rectangle rectangle = new Rectangle(new Point(6, 7), new Point(17, 1));
+
+        canvas.drawRectangle(rectangle);
+
+        // when
+        printer.print(canvas);
+
+        // then
+        assertThat(outContent.toString()).isEqualTo(
+                "------------" + System.lineSeparator() +
+                "|      x   |" + System.lineSeparator() +
+                "|      x   |" + System.lineSeparator() +
+                "|      x   |" + System.lineSeparator() +
+                "|      xxxx|" + System.lineSeparator() +
+                "|          |" + System.lineSeparator()  +
+                "------------" + System.lineSeparator() );
+    }
+
+    @Test
+    public void should_print_canvas_with_rectangle_and_vertical_and_horizontal_line() {
+        final Canvas canvas = new Canvas(10, 7);
+
+        final Line vertical = new Line(new Point(3, 0), new Point(3, 2));
+        final Line horizontal = new Line(new Point(0, 1), new Point(20, 1));
+        final Rectangle rectangle = new Rectangle(new Point(6, 6), new Point(9, 3));
+
+        canvas.drawLine(vertical);
+        canvas.drawLine(horizontal);
+        canvas.drawRectangle(rectangle);
+
+        // when
+        printer.print(canvas);
+
+        // then
+        assertThat(outContent.toString()).isEqualTo(
+                "------------" + System.lineSeparator() +
+                "|      xxxx|" + System.lineSeparator() +
+                "|      x  x|" + System.lineSeparator() +
+                "|      x  x|" + System.lineSeparator() +
+                "|      xxxx|" + System.lineSeparator() +
+                "|   x      |" + System.lineSeparator() +
                 "|xxxxxxxxxx|" + System.lineSeparator() +
                 "|   x      |" + System.lineSeparator()  +
                 "------------" + System.lineSeparator() );
